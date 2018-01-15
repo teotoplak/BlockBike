25ed8f68db4ad56061eb6badb3f8ecfe6c7b4a6d
teotoplak <teotoplak95@gmail.com>
2018-01-15 14:40:43 +0100

    rental (1)

--- contracts/Rentals.sol
+++ contracts/Rentals.sol
@@ -8,7 +8,28 @@ contract Rentals {
         bytes32 name;
     }
 
+    struct Rental {
+        uint deadline;
+        uint bikeId;
+        address renter;
+    }
+
+    mapping (address => uint) balances;
+
+
+    address owner;
+
     Bike[] public registered;
+    Rental[] public rentals;
+
+    function Rentals() public {
+        owner = msg.sender;
+    }
+
+    function giveMoney(uint amount, address receiver) public {
+        require(owner == msg.sender);
+        balances[receiver] += amount;
+    }
 
     function register(uint price, bytes32 name) public {
         registered.push(Bike({
@@ -18,8 +39,39 @@ contract Rentals {
         }));
     }
 
-    function listNames() returns(uint) {
-        return registered.length;
+    function rent(uint id, uint time) public returns (bool) {
+        require(id + 1 >= registered.length);
+        var bike = registered[id];
+        // todo consider time here
+        var totalPrice = bike.price;
+        // approx. enough money on sender
+        require( totalPrice < balances[msg.sender]);
+        // bike must be free for rental
+        for (uint i = 0; i < rentals.length; i++) {
+            if(rentals[i].bikeId == id) {
+                return false;
+            }
+        }
+        var futureDeadline = now + time;
+        rentals.push(Rental({
+            deadline: futureDeadline,
+            bikeId: id,
+            renter: msg.sender
+        }));
+        balances[msg.sender] -= totalPrice;
+        balances[bike.owner] += totalPrice;
+        return true;
+    }
+
+
+
+    function priceForBike(uint id) public view returns (uint) {
+        require(id + 1 >= registered.length);
+        return registered[id].price;
+    }
+
+    function checkBalance(address addr) public view returns (uint) {
+        return balances[addr];
     }
 
 }
--- test/rental.js
+++ test/rental.js
@@ -1,3 +1,45 @@
-/**
- * Created by teo on 1/11/18.
- */
+// Specifically request an abstraction for MetaCoin
+var Rentals = artifacts.require("Rentals");
+
+contract('Rentals', function(accounts) {
+    var instance;
+    it("should give 1000 to second user", function() {
+        return Rentals.deployed().then(function(inst) {
+            instance = inst;
+            return instance.giveMoney(1000,web3.eth.accounts[1])
+        }).then(function(result) {
+            return instance.checkBalance(web3.eth.accounts[1]);
+        }).then(function(result) {
+            assert.equal(result.valueOf(), 1000, "not 1000 after transfer")
+        })
+    });
+    it("should give 2000 to owner", function() {
+        return instance.giveMoney(2000,web3.eth.accounts[0])
+            .then(function(result) {
+                return instance.checkBalance(web3.eth.accounts[0]);
+        }).then(function(result) {
+            assert.equal(result.valueOf(), 2000, "not 2000 after transfer")
+        })
+    });
+    it("should register new bike (price 50) with second account", function() {
+        return instance.register(50,'HellRide', {from: web3.eth.accounts[1]})
+            .then(function(result) {
+                return instance.priceForBike(0);
+        }).then(function(result) {
+            assert.equal(result.valueOf(), 50, "not price 50 after register")
+        })
+    });
+    it("rent registered bike", function() {
+        return instance.rent(0, 60, {from: web3.eth.accounts[0]})
+            .then(function(result) {
+                return instance.checkBalance(web3.eth.accounts[0]);
+        }).then(function(result) {
+            assert.equal(result.valueOf(), 1950, "renter does not correspond!")
+        }).then(function(result) {
+                return instance.checkBalance(web3.eth.accounts[1]);
+        }).then(function(result) {
+            assert.equal(result.valueOf(), 1050, "renter does not correspond!")
+        })
+    });
+
+});
